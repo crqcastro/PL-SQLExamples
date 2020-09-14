bd8e48f97379c3565ce3d314a8233be81f19858f
Cesar Castro <cesarrqc@gmail.com>
2020-09-13 21:09:57 -0300

    Concluido REGEXP_COUNT e REGEXP_LIKE. Ainda pendende REGEXP_INSTR e REGEXP_SUBSTR

--- REGEX/REGEX.sql
+++ REGEX/REGEX.sql
@@ -1,23 +1,65 @@
-REGEXP_COUNT
-Conta a quantidade de ocorrÃªncias da expressÃ£o no texto procurado
+/*
++-----------------------------------------------------------------------------------------------------+
+| REGEXP_COUNT                                                                                        |
+| Conta a quantidade de ocorrências da expressão no texto procurado                                   |
++-----------------------------------------------------------------------------------------------------+
+| REGEXP_COUNT(<Texto informado>, <regex>, posição do inicio do teste, parametro)                     |
+| Parametros:                                                                                         |
+| >> 'i' Case-insensitive                                                                             |
+| >> 'c' case-sensitive                                                                               |
+| >> 'n' permite que o ponto (.), que é o caractere de correspondência de qualquer caractere,         |
+| corresponda ao caractere de nova linha. Se você omitir esse parâmetro, o ponto não corresponde      |
+| ao caractere de nova linha.                                                                         |
+| >> 'm' trata a string de origem como várias linhas. O Oracle interpreta o acento circunflexo (^)    |
+| e o cifrão ($) como o início e o fim, respectivamente, de qualquer linha em qualquer lugar na       |
+| string de origem, em vez de apenas no início ou no final de toda a string de origem. Se você omitir |
+| esse parâmetro, o Oracle tratará a string de origem como uma única linha.                           |
+| >> 'x' ignora caracteres de espaço em branco. Por padrão, os caracteres de espaço em branco         |
+| correspondem a si mesmos.                                                                           |
+| ** Parametro não é obrigatório **                                                                   |
++-----------------------------------------------------------------------------------------------------+
+| Ex:                                                                                                 |
+| select REGEXP_COUNT('554733251368', '(55[1-9]{2}[2,3,4,5]{1}[0-9]{7})', 1, 'i') from dual           |
+| >> Resultado: 1                                                                                     |
++-----------------------------------------------------------------------------------------------------+
+*/
+select REGEXP_COUNT('554733251368', '(55[1-9]{2}[2,3,4,5]{1}[0-9]{7})', 1, 'i') from dual;
 
-REGEXP_COUNT(<Texto informado>, <regex>, posiÃ§Ã£o do inicio do teste, parametro)
-Parametors:
-'i' Case-insensitive
-'c' case-sensitive
-'n' permite que o ponto (.), que Ã© o caractere de correspondÃªncia de qualquer caractere, corresponda ao caractere de nova linha. Se vocÃª omitir esse parÃ¢metro, o ponto nÃ£o corresponde ao caractere de nova linha.
-'m' trata a string de origem como vÃ¡rias linhas. O Oracle interpreta o acento circunflexo (^) e o cifrÃ£o ($) como o inÃ­cio e o fim, respectivamente, de qualquer linha em qualquer lugar na string de origem, em vez de apenas no inÃ­cio ou no final de toda a string de origem. Se vocÃª omitir esse parÃ¢metro, o Oracle tratarÃ¡ a string de origem como uma Ãºnica linha.
-'x' ignora caracteres de espaÃ§o em branco. Por padrÃ£o, os caracteres de espaÃ§o em branco correspondem a si mesmos.
+/*
++-----------------------------------------------------------------------------------------------------+
+| REGEXP_LIKE                                                                                         |
+| Utilizada para verificar, nas clausula where, a ocorrência da expressão no                          |
+| texto da coluna informada                                                                           |
++-----------------------------------------------------------------------------------------------------+
+| >> Usado na clausula Where de forma similar ao like.                                                |
+| REGEXP_LIKE(<Texto informado>, <regex>, parametro)                                                  |
+| Parametros                                                                                          |
+| >> 'i' Case-insensitive                                                                             |
+| >> 'c' case-sensitive                                                                               |
+| >> 'n' permite que o ponto (.), que é o caractere de correspondência de qualquer caractere,         |
+| corresponda ao caractere de nova linha. Se você omitir esse parâmetro, o ponto não corresponde      |
+| ao caractere de nova linha.                                                                         |
+| >> 'm' trata a string de origem como várias linhas. O Oracle interpreta o acento circunflexo (^)    |
+| e o cifrão ($) como o início e o fim, respectivamente, de qualquer linha em qualquer lugar na       |
+| string de origem, em vez de apenas no início ou no final de toda a string de origem. Se você omitir |
+| esse parâmetro, o Oracle tratará a string de origem como uma única linha.                           |
+| ** Parametro não é obrigatório **                                                                   |
++-----------------------------------------------------------------------------------------------------+
+| Ex:                                                                                                 |
+| select username, useremail from users                                                               |
+| where REGEXP_LIKE(useremail, '[a-z0-9.]+@hotmail.com', 'i')                                         |
++-----------------------------------------------------------------------------------------------------+
+*/
+create table users (username varchar2(20),email varchar2(100));
+insert into users values ('Cesar Castro', 'cesarrqc@gmail.com');
+insert into users values ('Mickey Mouse', 'mickey28@hotmail.com');
+select * from users;
+select * from users where REGEXP_LIKE (email, '[a-z0-9]+@hotmail.com', 'i');
 
-Ex:
-REGEXP_COUNT('554733251368', '(55[1-9]{2}[2,3,4,5]{1}[0-9]{7})', 1, 'i')
-Resultado: 1
-
-
-REGEXP_LIKE
-Utilizada para verificar, nas clausula where, a ocorrÃªncia da expressÃ£o no texto da coluna informada;
-
-
-REGEXP_REPLACE
+/*
++-----------------------------------------------------------------------------------------------------+
+| REGEXP_REPLACE                                                                                      |
+| 
 REGEXP_INSTR
 REGEXP_SUBSTR
+*/
